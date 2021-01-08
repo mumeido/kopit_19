af5928a0f3fad5bf6635be11555fbe1f84858ee0
Muhammad Syahrul Haniawan <72645722+mumeido@users.noreply.github.com>
2021-01-08 15:25:01 +0800

    Update kopit_19.py
    Menambah Jumlah kasus Provinsi

--- kopit_19.py
+++ kopit_19.py
@@ -5,150 +5,187 @@ import telepot
 import json
 import requests
 
+
 def action(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
     print('Received: %s' % command)
-    
+
     if command == '/start':
         telegram_bot.sendMessage(chat_id, "Welcome to COVID-19 Bot. \n \nThis bot will show you about COVID-19 information arround the globe. This bot can give you the number of confirmed, death, actived, and recovered in case of COVID-19 in many country. \n \nTo use this bot you can type /(country name) \nex: /indonesia, /germany, /greece, ect. \n \nThis data was get from kawalcorona.com.")
 
-    elif command == '/amerika':
-        id = 0
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + 
-        str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
     elif command == '/indonesia':
         id = 19
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + 
-        str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/japan':
-        id = 41
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) +
-        str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/india':
+        telegram_bot.sendMessage(chat_id, nameIndo(id) + str('\n') + confirmedIndo(id) +
+                                 str('\n') + deathIndo(id) + str('\n') + activeIndo(id) + str('\n') + recoveredIndo(id))
+    elif command == '/jakarta':
+        id = 0
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                    deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/jawabarat':
         id = 1
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/greece':
-        id = 62
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/egypt':
-        id = 63
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/malaysia':
-        id = 72
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/china':
-        id = 78
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/southkorea':
-        id = 85
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/australia':
-        id = 79
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/uruguay':
-        id = 88
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/thailand':
-        id = 111
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/iceland':
-        id = 121
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/vietnam':
-        id = 139
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/brunei':
-        id = 157
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/brazil':
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/jawatimur':
         id = 2
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/russia':
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/jawatengah':
         id = 3
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/france':
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/sulawesiselatan':
         id = 4
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/uk':
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/kalimantantimur':
         id = 5
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/turkey':
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/riau':
         id = 6
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/italy':
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/sumaterabarat':
         id = 7
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/spain':
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/banten':
         id = 8
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/germany':
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/sumaterautara':
         id = 9
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/argentina':
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/bali':
+        id = 10
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/kalimantanselatan':
         id = 11
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/mexico':
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/jogja':
         id = 12
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/poland':
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/papua':
         id = 13
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/southafrica':
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/sumateraselatan':
+        id = 14
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/kalimantantengah':
+        id = 15
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/sulawesiutara':
         id = 16
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/netherlands':
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/aceh':
+        id = 17
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/sulawesitenggara':
         id = 18
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
-    elif command == '/belgium':
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/kepulauanriau':
+        id = 19
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/lampung':
+        id = 20
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/papuabarat':
         id = 21
-        telegram_bot.sendMessage(chat_id, name(id) + str('\n') + confirmed(id) + str('\n') + death(id) + str('\n') + active(id) + str('\n') + recovered(id))
-
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/ntb':
+        id = 22
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/maluku':
+        id = 23
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/kalimantanutara':
+        id = 24
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/sulawesitengah':
+        id = 25
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/bengkulu':
+        id = 26
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/gorontalo':
+        id = 27
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/jambi':
+        id = 28
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/kalimantanbarat':
+        id = 29
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/malukuutara':
+        id = 30
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/bangkabelitung':
+        id = 31
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/ntt':
+        id = 32
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
+    elif command == '/sulawesibarat':
+        id = 33
+        telegram_bot.sendMessage(chat_id, nameProvi(id) + str('\n') + confirmedProvi(id) + str('\n') +
+                                 deathProvi(id) + str('\n') + recoverdProvi(id))
     else:
-        telegram_bot.sendMessage(chat_id, "Please type the right keyword. To use this bot you can type /(country name) \nex: /indonesia, /germany, /greece, ect. If the information of country that you want to know isn't display, don't shy to contact us.")
+        telegram_bot.sendMessage(
+            chat_id, "Please type the right keyword. To use this bot you can type /(country name) \nex: /indonesia, /germany, /greece, ect. If the information of country that you want to know isn't display, don't shy to contact us.")
 
-def name(id):
+
+def nameIndo(id):
     return "Country : " + dataJson[id]['attributes']['Country_Region']
 
-def confirmed(id):
+def nameProvi(id):
+    return "Provinsi : " + dataProvi[id]['attributes']['Provinsi']
+
+def confirmedIndo(id):
     return "Confirmed : " + str(dataJson[id]['attributes']['Confirmed'])
 
-def death(id):
+def confirmedProvi(id):
+    return "Confirmed : " + str(dataProvi[id]['attributes']['Kasus_Posi'])
+
+def deathIndo(id):
     return "Deaths : " + str(dataJson[id]['attributes']['Deaths'])
 
-def active(id):
+def deathProvi(id):
+    return "Deaths : " + str(dataProvi[id]['attributes']['Kasus_Meni'])
+
+def activeIndo(id):
     return "Active : " + str(dataJson[id]['attributes']['Active'])
 
-def recovered(id):
+
+def recoveredIndo(id):
     return "Recovered : " + str(dataJson[id]['attributes']['Recovered'])
+
+def recoverdProvi(id):
+    return "Recovered : " + str(dataProvi[id]['attributes']['Kasus_Semb'])
 
 
 now = datetime.datetime.now()
@@ -157,7 +194,11 @@ r = requests.get('https://api.kawalcorona.com/')
 data = r.text
 dataJson = json.loads(data)
 
-telegram_bot = telepot.Bot('UR API KEY')
+j = requests.get('https://api.kawalcorona.com/indonesia/provinsi/')
+data2 = j.text
+dataProvi = json.loads(data2)
+
+telegram_bot = telepot.Bot('1456306147:AAHWlYqBhKRiwOTsyKngDS7R4TcMf3ZrUUM')
 print(telegram_bot.getMe)
 MessageLoop(telegram_bot, action).run_as_thread()
 print('Bot sedang berjalan...')
